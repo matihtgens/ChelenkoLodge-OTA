a2a09a87c8a7f946203843c5d8a9e2844644d415
Matias Henriquez <matias.henriquez@genstudents.org>
2024-10-15 17:26:13 -0300

    header y nav responsivos con boostrap/grid, en proceso (matias)

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 function Header({ onCartClick }) {
     return (
         <header>
+            {/* NAVBAR SIN BOOSTRAP DEL QUE ME BASE , ELIMINARLO AL FINAL
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
                     <div className="d-flex justify-content-between w-100">
@@ -21,12 +22,40 @@ function Header({ onCartClick }) {
                                     <Link className="nav-link" to="/register"><button className="btn btn-outline-success my-2 my-sm-0" type="registro">Registro</button></Link>
                                 </div>
                                 <div className="links icono">
-                                <Link className="nav-link" to="/profile"><i className="bi bi-person-square"></i></Link>
+                                    <Link className="nav-link" to="/profile"><i className="bi bi-person-square"></i></Link>
                                 </div>
                                 <div className="links icono">
-                                <Link className="nav-link" to="/cart"><i className="bi bi-bag"></i></Link>
+                                    <Link className="nav-link" to="/cart"><i className="bi bi-bag"></i></Link>
                                 </div> 
                             </div>                                      
+                        </div>
+                    </div>
+                </div>
+            </nav>
+            */}
+
+            {/* NAVBAR CON BOOSTRAP SACADO DE LA MISMA DOCUMENTACION , EL BUTON ES EL QUE HACE LA MAGIA MAS LAS CLASES DEL BOOSTRAP */}
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="/home"><img src={logo} style={{ width: '100px' }} alt="Logotipo de la OTA" /></a>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarNavAltMarkup"
+                        aria-controls="navbarNavAltMarkup"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav">
+                            {/* JENNY ACA CAMBIAR LA RUTA Y NOS NOMBRES DE LAS PAGES, A CONTACTO, GALERIA, PERFIL, ETC ETC */}
+                            <a><Link className="nav-link" to="/home">Home</Link></a>
+                            <a><Link className="nav-link" to="/register">Registro</Link></a>
+                            <a><Link className="nav-link" to="/home">Contacto</Link></a>
+                            <a><Link className="nav-link" to="/home">Galeria</Link></a>                         
                         </div>
                     </div>
                 </div>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -13,34 +13,16 @@ function Navbar({ toggleCart }) { // Recibe toggleCart como prop
                 <div className="container">
                     <div className="navbar">
                         <form onSubmit={handleSubmit} className="row formulario">
-                            <div className="col-12 col-lg-3 col-md-6 col-sm-6 my-2">
+                            <div className="col-12 col-lg-5 col-md-6 col-sm-6 my-2 divUno">
                                 <label htmlFor="checkin" className="form-label">Entrada:</label>
                                 <input type="date" id="checkin" name="checkin" className="form-control" required />
                             </div>
-                            <div className="col-12 col-lg-3 col-md-6 col-sm-6 my-2">
+                            <div className="col-12 col-lg-5 col-md-6 col-sm-6 my-2">
                                 <label htmlFor="checkout" className="form-label">Salida:</label>
                                 <input type="date" id="checkout" name="checkout" className="form-control" required />
                             </div>
 
-                            <div className="col-6 col-lg-2 col-md-6 col-sm-6 my-2">
-                                <label htmlFor="adults" className="form-label">Adultos:</label>
-                                <select id="adults" name="adults" className="form-select" required>
-                                    <option value="1">1</option>
-                                    <option value="2">2</option>
-                                    <option value="3">3</option>
-                                    <option value="4">4</option>
-                                </select>
-                            </div>
-
-                            <div className="col-6 col-lg-2 col-md-6 col-sm-6 my-2">
-                                <label htmlFor="children" className="form-label">Niños:</label>
-                                <select id="children" name="children" className="form-select">
-                                    <option value="0">0</option>
-                                    <option value="1">1</option>
-                                    <option value="2">2</option>
-                                    <option value="3">3</option>
-                                </select>
-                            </div>
+                            
 
                             <div className="col-lg-2 my-2  buscar">
                                 <button className="btn btn-outline-success" type="submit">Buscar</button>
