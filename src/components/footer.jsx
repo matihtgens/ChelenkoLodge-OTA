ba4fe8caf946a99dc1011e6de415ba8569e7244b
Matias Henriquez <matias.henriquez@genstudents.org>
2024-10-22 18:03:04 -0300

    cambios en la tipografia

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './footer.css';
-import logo from '../assets/img/logo-black.webp';
+import logo from '../assets/img/logo-Black.webp';
 import flecha from '../assets/img/circulo-de-flecha.png'
 import { Link } from 'react-router-dom';
 
